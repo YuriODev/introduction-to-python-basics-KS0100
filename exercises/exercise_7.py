06d76e4992551737fc1226295fb7d88b99eaa5e6
KS0100 <keavanseev@gmail.com>
2024-06-17 09:47:49 +0100

    Update exercise_7.py

--- exercises/exercise_7.py
+++ exercises/exercise_7.py
@@ -1,7 +1,7 @@
-x = int(input())
-n1 = x//1000
-n2 = (x % 1000)//100
-n3 = (x % 100)//10
-n4 = x % 10
-sum_of_digits =n1+n2+n3+n4
-print(sum_of_digits)
+n = int(input("Enter a 4 digit number?"))
+d1 = n//1000
+d2 = (n%1000) // 100
+d3 = (n%100) // 10
+d4 = n%10
+sum_of_digits =d1+d2+d3+d4
+print("sum_of_digits =", sum_of_digits)
